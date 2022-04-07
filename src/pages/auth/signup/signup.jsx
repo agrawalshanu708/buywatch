3d35ff7157004370f8e036f6de17c43603a0b3a4
shanu agrawal <agrawalshanu798@gmail.com>
2022-04-07 14:47:39 +0530

    feat: add signup auth

--- src/pages/auth/signup/signup.jsx
+++ src/pages/auth/signup/signup.jsx
@@ -1,63 +1,119 @@
-import React from 'react'
-import "./signup.css"
-import {signup1} from "./../../../Assets/index"
+import React from "react";
+import { useState } from "react";
+import axios from "axios"
+import "./signup.css";
+import { signup1 } from "./../../../Assets/index";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
-  return (
-    <>
-    <div className="outer-box" style={{backgroundImage: 'url('+signup1+')'}} >
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [password, setPassword] = useState("");
+const navigate = useNavigate()
+const signupHandler = async () => {
 
-     <div className="signup-form col-5">
-         <h2>Register.</h2>
-         <p>Fill your details here</p>
-
-    <form>
-    <div class="grid-container">
-
-    <div class="grid-item">
-     <label htmlFor="user-first-name">First Name</label>
-    <input id="user-first-name" type="text" />
-    </div>
-
-  <div class="grid-item">
-  <label htmlFor="user-last-name">Last Name</label>
-   <input id="user-last-name" type="text" />
-  </div>
-
-  <div class="grid-item">
-  <label htmlFor="user-signup-email">Email</label>
-  <input id="user-signup-email" type="email" />
-  </div>  
-
-  <div class="grid-item">
-  <label htmlFor="user-number">Contact Number</label>
-  <input id="user-number" type="number" />
-  </div>
-
-  <div class="grid-item">
-  <label htmlFor="user-pass">Create Password</label>
-  <input id="user-pass" type="password" />
-  </div>
-
-  <div class="grid-item">
-  <label htmlFor="user-pass-again">Confirm Password</label>
- <input htmlFor="user-pass-again" type="password" /></div>  
-</div>
-
-<div class="condition-box">
-<input id="user-condition" type="checkbox"/>
-<label htmlFor="user-condition" class="condition-tag">Yes, I agree to all the Terms, Privacy policy</label>
-</div>
-
-<div className="button-box">
-<button className="register-button">Register</button>
-</div>
-</form>
-
-           
-     </div>
-     </div>
-    </>
-  )
+const body = {
+  email: email,
+  password: password,
+  firstName: firstName,
+  lastName: lastName,
+}
+ 
+try {
+ const response = await axios.post("/api/auth/signup", body)
+ localStorage.setItem("token", response.data.encodedToken);
+ setTimeout(() => {
+  navigate("/login")
+ }, 1000);
+} catch (error) {
+  console.error("error")
+}
 }
 
-export {Signup}
+
+  return (
+    <>
+      <div
+        className="outer-box"
+        style={{ backgroundImage: "url(" + signup1 + ")" }}
+      >
+        <div className="signup-form col-5">
+          <h2>Register.</h2>
+          <p>Fill your details here</p>
+
+ 
+            <div class="grid-container">
+              <div class="grid-item">
+                <label htmlFor="user-first-name">First Name</label>
+                <input
+                  id="user-first-name"
+                  type="text"
+                  onChange={(e) => {
+                    e.preventDefault()
+                    setFirstName(e.target.value)}}
+                />
+              </div>
+
+              <div class="grid-item">
+                <label htmlFor="user-last-name">Last Name</label>
+                <input
+                  id="user-last-name"
+                  type="text"
+                  onChange={(e) => {
+                    e.preventDefault()
+                    setLastName(e.target.value)}}
+                />
+              </div>
+
+              <div class="grid-item">
+                <label htmlFor="user-signup-email">Email</label>
+                <input
+                  id="user-signup-email"
+                  type="email"
+                  onChange={(e) => {
+                    e.preventDefault()
+                    setEmail(e.target.value)}}
+                />
+              </div>
+
+              {/* <div class="grid-item">
+  <label htmlFor="user-number">Contact Number</label>
+  <input id="user-number" type="number" />
+  </div> */}
+
+              <div class="grid-item">
+                <label htmlFor="user-pass">Create Password</label>
+                <input
+                  id="user-pass"
+                  type="password"
+                  onChange={(e) => {
+                    e.preventDefault()
+                    setPassword(e.target.value)}}
+                />
+              </div>
+
+              {/* <div class="grid-item">
+  <label htmlFor="user-pass-again">Confirm Password</label>
+ <input htmlFor="user-pass-again" type="password" /></div>   */}
+            </div>
+
+            <div class="condition-box">
+              <input id="user-condition" type="checkbox" />
+              <label htmlFor="user-condition" class="condition-tag">
+                Yes, I agree to all the Terms, Privacy policy
+              </label>
+            </div>
+
+            <div className="button-box">
+              <button className="register-button"
+              onClick={() => signupHandler()}
+              >Register</button>
+            </div>
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Signup };
