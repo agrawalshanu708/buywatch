84a51f61d15fed6f7c2f0a9a9013f59c94bcda88
shanu agrawal <agrawalshanu798@gmail.com>
2022-05-13 19:31:21 +0530

    fix: css of error page

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Error404.css";
 const Error404 = () => {
   return (
-    <div style = {{width: '50%',margin:'auto', display:'flex',flexDirection: 'column', 
-    gap: '1rem', alignItems: 'center', marginTop:"7rem"
-    }}>
-    <h1>Opps! Page Not Found</h1>
-    <p style = {{textAlign: 'center'}}>You must have picked the wrong door because I haven't been able to lay my eye on the page you've been searching for.</p>
-   <Link to = "/"><button className="btn">Back TO Home</button></Link> 
+    <div className="error__box">
+      <h1>Opps! Page Not Found</h1>
+      <p className="error__p">
+        You must have picked the wrong door because I haven't been able to lay
+        my eye on the page you've been searching for.
+      </p>
+      <Link to="/">
+        <button className="btn">Back TO Home</button>
+      </Link>
+    </div>
+  );
+};
 
-   </div>
-  )
-}
-
-export {Error404}
+export { Error404 };
